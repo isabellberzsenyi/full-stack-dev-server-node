37a83649442e0f248b56dbf7707e97a2659449e9
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2022-04-06 17:03:12 -0400

    adding mongodb uri

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ import helloController from "./controllers/hello-controller.js";
 import tuitController from './controllers/tuit-controller.js';
 import userController from './controllers/user-controller.js';
 
-mongoose.connect('mongodb://localhost:27017/webdev');
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/webdev';
+
+mongoose.connect(uri);
 const app = express();
 app.use(express.json());
 app.use(cors());
